59d9f4ace8682eb67c2312ef8de10d48406cc773
Euler_s_identity <euler_s_identity@Euler-s-identitys-MacBook-Pro.local>
2020-03-21 18:11:54 -0700

    finished working simple web server tested

--- server.py
+++ server.py
@@ -10,34 +10,34 @@ serverSocket.listen(1) # TCP has listen()
 print 'the web server is on port:', serverPort
 #Fill in end
 while True:
-	#Establish the connection
+#Establish the connection
 	print 'Ready to serve...'
-	connectionSocket, addr = serverSocket.accept() #accept and initialize another socket - 'connection_socket'
+	connectionSocket, addr = serverSocket.accept()
 	try:
 		#Fill in start
-		message = connectionSocket.recv(bufferSize)
-		print message, '::',message.split()[0],':',message.split()[1]
-		filename = message.split[1]
-		print filename, '||',filename[1:]
+			message = connectionSocket.recv(bufferSize)
+			filename = message.split()[1]
 		#Fill in end
-		f = open(filename[1:])
-		#Fill in start 
-		outputdata = f.read()
-		#Fill in end 
+			f = open(filename[1:])
+			outputdata = f.read()#Fill in start #Fill in end 
+			print outputdata
+			f.close()
 		#Send one HTTP header line into socket
 		#Fill in start
-		connectionSocket.send('\nHTTP/1.1 200 OK\n\n')
-		connectionSocket.send(outputdata)
+			connectionSocket.send(bytes('HTTP/1.1 200 OK\r\n\r\n')) #need to in the correct format
+			#connectionSocket.send(outputdata)
 		#Fill in end
 		#Send the content of the requested file to the client
-     	for i in range(0, len(outputdata)):
-        	connectionSocket.send(outputdata[i])
-     	connectionSocket.close()
+     			for i in range(0, len(outputdata)):
+         			connectionSocket.send(outputdata[i])
+     			connectionSocket.close()
+
 	except IOError:
-	#Send response message for file not found
+    #Send response message for file not found
 	#Fill in start 
-	connectionSocket.send('\nHTTP/1.1 404 Not Found\n\n')
-	connectionSocket.send('\nHTTP/1.1 404 Not Found\n\n')
+			connectionSocket.send(bytes("HTTP/1.1 404 Not Found\r\n\r\n"))
+			connectionSocket.send("<html><head></head><body><h1>404 Not found</h1></body></html>\r\n")
+			connectionSocket.close()
 	#Fill in end
-	#Close client socket #Fill in start #Fill in end
+#Close client socket #Fill in start #Fill in end
 serverSocket.close()
